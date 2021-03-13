6f3b5551c95871bc21006553256a1ce92b27bf18
By_JumperX4 <byjumperx4@gmail.com>
2021-03-13 01:20:16 +0100

    Migrate to Funtoo XFCE

---
+++ .config/awesome/lib/wibox/widget/textclock.lua
@@ -0,0 +1,62 @@
+---------------------------------------------------------------------------
+--- Text clock widget.
+--
+-- @author Julien Danjou &lt;julien@danjou.info&gt;
+-- @copyright 2009 Julien Danjou
+-- @classmod wibox.widget.textclock
+---------------------------------------------------------------------------
+
+local setmetatable = setmetatable
+local os = os
+local textbox = require("wibox.widget.textbox")
+local timer = require("gears.timer")
+local glib = require("lgi").GLib
+local DateTime = glib.DateTime
+local TimeZone = glib.TimeZone
+
+local textclock = { mt = {} }
+
+--- This lowers the timeout so that it occurs "correctly". For example, a timeout
+-- of 60 is rounded so that it occurs the next time the clock reads ":00 seconds".
+local function calc_timeout(real_timeout)
+    return real_timeout - os.time() % real_timeout
+end
+
+--- Create a textclock widget. It draws the time it is in a textbox.
+--
+-- @tparam[opt=" %a %b %d, %H:%M "] string format The time format.
+-- @tparam[opt=60] number timeout How often update the time (in seconds).
+-- @tparam[opt=local timezone] string timezone The timezone to use,
+--   e.g. "Z" for UTC, "±hh:mm" or "Europe/Amsterdam". See
+--   https://developer.gnome.org/glib/stable/glib-GTimeZone.html#g-time-zone-new.
+-- @treturn table A textbox widget.
+-- @function wibox.widget.textclock
+function textclock.new(format, timeout, timezone)
+    format = format or " %a %b %d, %H:%M "
+    timeout = timeout or 60
+    timezone = timezone and TimeZone.new(timezone) or TimeZone.new_local()
+
+    local w = textbox()
+    local t
+    function w._private.textclock_update_cb()
+        w:set_markup(DateTime.new_now(timezone):format(format))
+        t.timeout = calc_timeout(timeout)
+        t:again()
+        return true -- Continue the timer
+    end
+    t = timer.weak_start_new(timeout, w._private.textclock_update_cb)
+    t:emit_signal("timeout")
+    return w
+end
+
+function textclock.mt:__call(...)
+    return textclock.new(...)
+end
+
+--
+
+--
+
+return setmetatable(textclock, textclock.mt)
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
---
+++ README.md
@@ -0,0 +1,76 @@
+# dotfiles
+My dotfiles. Fell free to use them and fork them.
+
+Warning : commented with ass and partially in frenglish (french + english). May be impossible to understand.
+
+My setup :
+
+Distro : Bedrock Linux with Slackware 14.2 as main stratum, hijacked on Oracle Linux 8.3
+
+CPUs : 2x Intel Xeon X5680
+
+GPU : PNY nVidia GTX 1060 6GB
+
+GPU Driver : nVidia proprietary driver
+
+RAM : 108 GB of ram
+
+Locale : fr_BE.UTF-8
+
+Keymap : us-altgr-intl
+
+Screens setup :
+
+1x vertical 1920x1080 screen (left)
+
+1x horizontal 2560x1440 screen (middle)
+
+1x horizontal 1360x768 screen (right)
+
+1x horizontal 1920x1080 graphics tablet (in front of the middle screen, middle screen is higher so it doesn't mask the screen)
+
+DE : XFCE (i'll upload my config some day)
+
+Browser : Ungoogled Chromium
+
+Editor : Vim & mousepad
+
+Terminal Emulator : alacritty
+
+Shell : bash
+
+Some useful details you may want :
+
+Some useful commands to do in your terminal and shortcuts :
+
+x :  exits
+
+fuck : do the command you've just entered but this time with doas
+
+clr : alias to clear, less letters
+
+l : ls -l
+
+xephinit : runs your, xinitrc in xephir (useful to configure a wm)
+
+kerncomp : compiles and installs the linux kernel (you must be in the folder of the kernel's sources, only works if genkernel is installed)
+
+traceroute4ever : useful to test if your internet works
+
+newtor : restart tor service and privoxy service to get a new tor identity
+
+Ctrl + Q : go to the begining of the line
+
+Ctrl + E : go to the end of the line
+
+Ctrl + R : search for a command in your history
+
+Vim shortcuts :
+
+Ctrl + S : save
+
+Ctrl + Q : quit
+
+Ctrl + Z : undo
+
+Ctrl + Y : redo
