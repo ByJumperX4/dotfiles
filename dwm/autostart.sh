60db5759d7a710ad565b6bd65903d5002a3b1a80
By_JumperX4 <>
2021-12-15 00:40:39 +0100

    Update

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -4,6 +4,7 @@ nitrogen --restore &                             # Wallpaper
 numlockx &                                       # Enable numlock
 firewall-applet &                                # GNU/Linux Firewalld applet
 nm-applet &                                      # For GNU/Linux NetworkManager
+freenet start &                                  # Start Freenet
 seapplet &                                       # SELinux alerts applet, if present
 pasystray -N sink_default -N source_default &    # Sound applet with notifications when something is changed 
 keepassxc &                                      # Password manager
